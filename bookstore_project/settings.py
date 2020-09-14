6bc483f7e0f4a9ff053639b3ab7dccde9d370e7f
Nicola Spanu <nicspanu@gmail.com>
2020-09-14 16:00:02 +0200

    second commit

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -150,3 +150,5 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
