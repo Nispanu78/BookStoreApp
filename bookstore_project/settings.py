7176a40a4a13f03531c468b0959989a7ed8a5bcb
Nicola Spanu <nicspanu@gmail.com>
2020-09-17 17:10:40 +0200

    third commit

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'books.apps.BooksConfig'
 ]
 
 MIDDLEWARE = [
@@ -151,4 +152,9 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = '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' # this is exactly the value 'apikey'
+# EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
--- users/views.py
+++ users/views.py
@@ -8,5 +8,3 @@ class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
-
-# Create your views here.
